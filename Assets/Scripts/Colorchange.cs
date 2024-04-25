d4a3be9e2dd25737e7d1e3a405c19b8f34c84d16
aub2012 <aubken2012@gmail.com>
2024-04-25 14:45:43 -0400

    First push

---
+++ Assets/Scripts/Colorchange.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colorchange : MonoBehaviour
+{
+    
+    
+    
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        // if (Input.GetMouseButton(0))
+        // {
+        //     if (Physics.Raycast(Eyes.transform.position, Eyes.transform.forward,
+        //             out RaycastHit hit, 10))
+        //     {
+        //         MeshRenderer mr = hit.collider.GetComponent<MeshRenderer>();
+        //         if (mr != null)
+        //         {
+        //             mr.material = Gray;
+        //         }
+        //     }
+        // }
+    }
+    
+}
