45a6095c513ede9df0c11b8e84153c25c15d5793
aub2012 <aubken2012@gmail.com>
2024-04-25 16:39:03 -0400

    Color change and movement

--- Assets/Scripts/Colorchange.cs
+++ Assets/Scripts/Colorchange.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 
 public class Colorchange : MonoBehaviour
 {
-    
-    
+
+    public Camera Eyes;
+    public Material Gray;
     
    
     void Start()
     {
         
     }
-
     
     void Update()
     {
-        // if (Input.GetMouseButton(0))
-        // {
-        //     if (Physics.Raycast(Eyes.transform.position, Eyes.transform.forward,
-        //             out RaycastHit hit, 10))
-        //     {
-        //         MeshRenderer mr = hit.collider.GetComponent<MeshRenderer>();
-        //         if (mr != null)
-        //         {
-        //             mr.material = Gray;
-        //         }
-        //     }
-        // }
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(Eyes.transform.position, Eyes.transform.forward,
+                    out RaycastHit hit, 5))
+            {
+                MeshRenderer[] meshes = hit.collider.GetComponentsInChildren<MeshRenderer>();
+                
+                foreach (MeshRenderer m in meshes)
+                {
+                    m.material = Gray;
+                }
+                
+            }
+        }
     }
     
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera Eyes;
+    public Rigidbody RB;
+    public float MouseSensitivity = 3;
+    public float WalkSpeed = 10;
+    public float JumpPower = 7;
+    public List<GameObject> Floors;
+    
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        float xRot = Input.GetAxis("Mouse X") * MouseSensitivity;
+        float yRot = -Input.GetAxis("Mouse Y") * MouseSensitivity;
+        transform.Rotate(0,xRot,0);
+        Eyes.transform.Rotate(yRot,0,0);
+
+        if (WalkSpeed > 0)
+        {
+            Vector3 move = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+                move += transform.forward;
+            if (Input.GetKey(KeyCode.S))
+                move -= transform.forward;
+            if (Input.GetKey(KeyCode.A))
+                move -= transform.right;
+            if (Input.GetKey(KeyCode.D))
+                move += transform.right;
+            move = move.normalized * WalkSpeed;
+            if (JumpPower > 0 && Input.GetKeyDown(KeyCode.Space) && OnGround())
+                move.y = JumpPower;
+            else
+                move.y = RB.velocity.y;
+            RB.velocity = move;
+        }
     }
+
+    public bool OnGround()
+    {
+        return Floors.Count > 0;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!Floors.Contains(other.gameObject))
+            Floors.Add(other.gameObject);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        Floors.Remove(other.gameObject);
+    }
+
 }
